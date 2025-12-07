938b1c0a9299d86bed96a55b444f552c103f53e8
Kapila Mane <114262608+km776@users.noreply.github.com>
2025-12-07 09:03:57 -0500

    Add files via upload

--- entity/document/passport.gd
+++ entity/document/passport.gd
@@ -14,31 +14,47 @@ func _ready() -> void:
 func populate_passport_data():
 	if character_data == null:
 		return
-	var gender = "M"
-	if character_data.gender == EnumAutoload.Gender.FEMALE:
-		gender = "F"
+	
+	var storage = ""
+	
+	if character_data.gender == EnumAutoload.Gender.OTHER:
+		storage = "Other"
+	
+	if character_data.gender == EnumAutoload.Gender.MANAGER:
+		storage = random_pwmanager(character_data.breached_manager, character_data.day3yes)
+		
+	# expiry date generation --> swap to password length
 	var expired_date = random_generate_date(character_data.birthdate, character_data.passport_expired)
-	basic_details.text = basic_details.text.format({"name": character_data.name, "dob": character_data.birthdate, "gender": gender, "expired_date": expired_date})
+	
+	basic_details.text = basic_details.text.format({"name": character_data.name, "dob": character_data.birthdate, "storage": storage, "expired_date": expired_date})
 	top_serial.text = character_data.id_number
 	face_photo.texture = character_data.face_photo
 
-func random_generate_date(birthdate: String, expired: bool) -> String:
+func random_generate_date(_birthdate: String, expired: bool) -> String:
 	# Create a random number generator
 	var rng = RandomNumberGenerator.new()
-	var day = rng.randi_range(1, 31)
-	var month = rng.randi_range(1, 12)
 
-	if month == 2:
-		if day > 28:
-			day = 28
-
-	var date_parts = birthdate.split("/")
-	var birthyear = int(date_parts[2])
-
-	var year = rng.randi_range(2001, 2020)
+	var year = rng.randi_range(12, 28)
 	if expired:
-		year = rng.randi_range(birthyear + 18, 1999)
+		year = rng.randi_range(6, 11)
 
-	var date_str = "%02d/%02d/%04d" % [day, month, year]
+	var date_str = str(year, " characters")
 
 	return date_str
+	
+# Create random pw manager selection
+func random_pwmanager(breached: bool, day3yes: bool) -> String:
+	var my_strings = ["FirstPass", "BitWardened", "1Key",
+	"KeeMaybe", "Safe-Ish", "TitanicPass"]
+	var random_string = my_strings.pick_random()
+	
+	if breached:
+			var breached_strings = ["KeeMaybe", "Safe-Ish", "TitanicPass"]
+			random_string = breached_strings.pick_random()
+	
+	if day3yes:
+			var safe_strings = ["FirstPass", "BitWardened", "1Key"]
+			random_string = safe_strings.pick_random()
+	
+	return random_string
+	
